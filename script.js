7184a0bd50e8cef75f78229d7d9fd53ae35c2893
Sam Greenwood <jammer2001@gmail.com>
2025-07-17 19:21:09 -0400

    fix sidebar button

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const geoJsonLayers = {}; // To store references to GeoJSON layers for filtering
 document.addEventListener("DOMContentLoaded", function () {
   const aside = document.querySelector("aside");
   const toggleBtn = document.getElementById("sidebarToggle");
-  const toggleBtnRight = document.getElementById("sidebarToggleRight");
   let collapsed = localStorage.getItem("sidebarCollapsed") === "true";
 
   // Create a floating reopen button
@@ -33,13 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
     collapsed = !collapsed;
     updateSidebarState();
   });
-
-  if (toggleBtnRight) {
-    toggleBtnRight.addEventListener("click", function () {
-      collapsed = !collapsed;
-      updateSidebarState();
-    });
-  }
 
   reopenBtn.addEventListener("click", function () {
     collapsed = false;
